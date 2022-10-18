f85646b8a0cdda8754dea8d109b402a5c968e075
Brian-CodeNation <brian.harkins@wearecodenation.com>
2022-10-18 12:09:32 +0100

    Add files

---
+++ example.js
@@ -0,0 +1,6 @@
+const myFunctions = require(`./modules/exp1.js`);
+
+console.log(myFunctions.add(2, 5));
+console.log(myFunctions.subtract(5, 2))
+console.log(myFunctions.multiply(5, 2))
+console.log(myFunctions.divide(5, 2))
